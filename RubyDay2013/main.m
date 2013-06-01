88387f8078e6ea5fb000b5db15cb04a373aa0433
Federico Ravasio <ravasio.federico@gmail.com>
2013-06-01 11:01:51 +0200

    First commit.

---
+++ RubyDay2013/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RubyDay2013
+//
+//  Created by Federico Ravasio on 6/1/13.
+//  Copyright (c) 2013 Webrain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RDAppDelegate class]));
+    }
+}
