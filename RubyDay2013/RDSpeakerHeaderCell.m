b0e443e7e9a6b23b0fa56c9f7d055afe7a4474fc
Federico Ravasio <ravasio.federico@gmail.com>
2013-06-05 15:17:03 +0200

    Bigger twitter avatars!

--- RubyDay2013/RDSpeakerHeaderCell.m
+++ RubyDay2013/RDSpeakerHeaderCell.m
@@ -38,7 +38,7 @@
     name.text = data[@"name"];
     handle.text = [NSString stringWithFormat:@"@%@", data[@"handle"]];
     
-    NSString *avatarUrl = [NSString stringWithFormat:@"http://api.twitter.com/1/users/profile_image?screen_name=%@&size=bigger", data[@"handle"]];
+    NSString *avatarUrl = [NSString stringWithFormat:@"http://api.twitter.com/1/users/profile_image?screen_name=%@&size=original", data[@"handle"]];
     [avatar setImageWithURL:[NSURL URLWithString:avatarUrl] placeholderImage:[UIImage imageNamed:@"rubylang"]];
 }
 
